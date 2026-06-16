9756ca4e0c1a3aeb139d2a340c582a4acc3586ef
Artha48 <92628344+Artha48@users.noreply.github.com>
2026-06-16 22:17:15 +0800

    Update API_URL to use ngrok URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://shortcake-opossum-splashy.ngrok-free.dev';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Check API status
